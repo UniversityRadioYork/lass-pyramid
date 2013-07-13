9237d0e797d04cd1b5e1f8595dca2a46ca811ac1
Matt "Hayashi" Windsor <matt.windsor@ury.york.ac.uk>
2013-07-13 16:04:23 +0100

    move filling code in views.py to schedule/lists.py

--- schedule/lists.py
+++ schedule/lists.py
@@ -1,13 +1,53 @@
 """Functions for selecting lists of timeslots based on certain criteria."""
 
-import sqlalchemy
 import datetime
+import sqlalchemy
 
 from . import models
 
+import lass.common.time
+import lass.schedule.filler
+
 
 def next(count, start_at=None):
+    """Selects the next 'count' shows, including the currently playing timeslot,
+    and performs filling and annotating.
+
+    Args:
+        count: The maximum number of timeslots to select.
+        start_at: The datetime used as a reference point to determine which
+            shows are the 'next' ones.  (Default: now)
+
+    Returns:
+        A list containing up to 'count' timeslots, which may either be actual
+        timeslots or filler.  The list represents the next 'count' timeslots
+        to occur from 'start_at', with any trailing filler limited to at most
+        one day in length.
+    """
+    if not start_at:
+        start_at = lass.common.time.aware_now
+
+    raw = lass.schedule.lists.next_raw(count, start_at)
+    if raw:
+        lass.schedule.models.Timeslot.annotate(raw)
+        start = min(raw[0].start_time, start_at)
+        end = raw[-1].end_time
+    else:
+        start = start_at
+        end = start_at
+
+    return lass.schedule.filler.fill(
+        raw,
+        lass.schedule.filler.filler_from_config(),
+        start_time=start,
+        end_time=end + datetime.timedelta(hours=24)
+    )[:count]
+
+
+def next_raw(count, start_at=None):
     """Selects the next 'count' shows, including the currently playing timeslot.
+
+    No filling is done.
 
     Args:
         count: The maximum number of timeslots to select.
@@ -19,7 +59,7 @@ def next(count, start_at=None):
         to occur from 'start_at' (or now if 'from' is None).
     """
     if not start_at:
-        start_at = datetime.datetime.now(datetime.timezone.utc)
+        start_at = lass.common.time.aware_now
 
     return models.Timeslot.query.filter(
         ((models.Timeslot.start_time + models.Timeslot.duration) > start_at)
--- views.py
+++ views.py
@@ -15,21 +15,7 @@ class MockSchedule(object):
     @property
     def timeslots(self):
         if not hasattr(self, '_timeslots'):
-            now = datetime.datetime.now(datetime.timezone.utc)
-            unfilled = lass.schedule.lists.next(10, start_at=now)
-            if unfilled:
-                lass.schedule.models.Timeslot.annotate(unfilled)
-                start = min(unfilled[0].start_time, now)
-                end = unfilled[-1].end_time
-            else:
-                start = now
-                end = now
-            self._timeslots = lass.schedule.filler.fill(
-                unfilled,
-                lass.schedule.filler.filler_from_config(),
-                start_time=start,
-                end_time=end + datetime.timedelta(hours=24)
-            )
+            self._timeslots = lass.schedule.lists.next(10)
         return self._timeslots
 
 
@@ -73,7 +59,6 @@ def standard_context(event):
     request = event['request']
 
     website = make_website(request)
-    static = lambda *args: request.static_url('/'.join((STATIC,) + args))
     try:
         current_url = pyramid.url.current_route_url(request)
     except ValueError:
@@ -88,48 +73,8 @@ def standard_context(event):
             'transmitting': True,
             'broadcasting': True,
             'website': website,
-            'static': static,
-            'url': request.route_url,
             'raw_url': lambda r: request.route_url('home') + r,
             'current_url': current_url,
             'page_title': get_page_title(request, current_url, website),
         }
     )
-
-
-@pyramid.view.view_config(
-    route_name='about',
-    renderer='website/about.jinja2'
-)
-@pyramid.view.view_config(
-    route_name='contact',
-    renderer='website/contact.jinja2'
-)
-@pyramid.view.view_config(
-    route_name='listen',
-    renderer='website/listen.jinja2'
-)
-def static(request):
-    """A view that can be used for static website pages.
-    """
-    return dict()
-
-
-@pyramid.view.view_config(
-    route_name='home',
-    renderer='website/index.jinja2'
-)
-def home(request):
-    return {
-        'page_title': 'Home'
-    }
-
-
-@pyramid.view.notfound_view_config(
-    renderer='errors/404.jinja2',
-    append_slash=True
-)
-def not_found(context, request):
-    return {
-        'page_title': 'Not Found'
-    }
