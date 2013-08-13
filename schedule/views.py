a957ea229f8c593b1732ef79c2bd4e567d88ae47
mattbw <matt.windsor@ury.york.ac.uk>
2013-08-13 20:22:16 +0100

    Implement the show search view code.
    Also some cleanup riders in the schedule views code file.

--- schedule/views.py
+++ schedule/views.py
@@ -1,3 +1,32 @@
+"""Views for the Schedule submodule.
+
+---
+
+Copyright (c) 2013, University Radio York.
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+* Redistributions of source code must retain the above copyright notice,
+  this list of conditions and the following disclaimer.
+* Redistributions in binary form must reproduce the above copyright
+  notice, this list of conditions and the following disclaimer in the
+  documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
 import datetime
 import functools
 import operator
@@ -7,6 +36,11 @@ import sqlalchemy
 import lass.credits.query
 import lass.model_base
 import lass.schedule.models
+
+
+#
+# SHOW DATABASE
+#
 
 
 @pyramid.view.view_config(
@@ -27,6 +61,21 @@ def shows(request):
     )
 
     return lass.common.view_helpers.media_list(request, source)
+
+
+@pyramid.view.view_config(
+    route_name='schedule-show-search',
+    renderer='schedule/show_search.jinja2'
+)
+def search(request):
+    """Performs a search if a query is given, or allows the user to do
+    so.
+    """
+    return lass.common.view_helpers.search(
+        request,
+        lass.schedule.models.Show,
+        lambda id: request.route_url('schedule-show-detail', showid=id)
+    )
 
 
 @pyramid.view.view_config(
